92c46566b96971cd39efe9689ca4acba1928218e
Luke Lau <luke_lau@icloud.com>
2020-02-21 14:25:12 +0000

    Try persisting now

--- move-to-firestore/main.py
+++ move-to-firestore/main.py
@@ -1,4 +1,7 @@
 import base64
+from google.cloud import firestore
+
+db = firestore.Client()
 
 def move_to_firestore(event, context):
     """Triggered from a message on a Cloud Pub/Sub topic.
@@ -7,5 +10,11 @@ def move_to_firestore(event, context):
          context (google.cloud.functions.Context): Metadata for the event.
     """
     pubsub_message = base64.b64decode(event['data']).decode('utf-8')
-    print('Heres a message')
+
+    doc_ref = db.collection(u'device-config').document()
+    doc_ref.set({
+        u'message': pubsub_message
+        })
+
+    print('Persisting a message')
     print(pubsub_message)
--- move-to-firestore/requirements.py
+++ move-to-firestore/requirements.py
@@ -1,3 +1,3 @@
 # Function dependencies, for example:
 # package>=version
-
+google-cloud-firestore>=1.6
