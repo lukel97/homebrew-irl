31e8ad8391cd99ffa8818d6e30f74da22599f804
Luke Lau <luke_lau@icloud.com>
2020-02-21 13:58:19 +0000

    Testing out the deploy again

--- move-to-firestore/main.py
+++ move-to-firestore/main.py
@@ -7,5 +7,5 @@ def move_to_firestore(event, context):
          context (google.cloud.functions.Context): Metadata for the event.
     """
     pubsub_message = base64.b64decode(event['data']).decode('utf-8')
-    print('heres a message')
+    print('Heres a message')
     print(pubsub_message)
