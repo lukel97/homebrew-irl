22b8b3e4591ce44147ee61a79fb57571070f410e
Luke Lau <luke@lukelau.me>
2020-04-02 01:07:13 +0000

    Fix messages not being sent to the socket

--- websocket-server/app.rb
+++ websocket-server/app.rb
@@ -8,19 +8,16 @@ sub = topic.subscription 'websocket'
 @socks = []
 
 subscriber = sub.listen do |msg|
+  puts "Got message #{msg.data}"
   for ws in @socks
     ws.send msg.data
   end
   msg.acknowledge!
 end
 
-subscriber.start
-
 port = ENV['PORT'] || 443
 
-puts "Now listening on port #{port}"
-
-EventMachine.run do
+EM.run do
   WebSocket::EventMachine::Server.start(:host => '0.0.0.0', :port => port) do |ws|
     ws.onopen do
       puts "Socket opened"
@@ -30,5 +27,10 @@ EventMachine.run do
       puts "Socket closed"
       @socks -= [ws]
     end
+    ws.onerror do |e|
+      puts "error #{e}"
+    end
   end
+  puts "Now listening on port #{port}"
+  subscriber.start
 end
