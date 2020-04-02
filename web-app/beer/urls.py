d917929de482dd9548a03166ea43799adcc92bf8
Luke Lau <luke@lukelau.me>
2020-04-02 14:51:04 +0000

    Fix some paths for deployment on digitalocean

--- web-app/beer/urls.py
+++ web-app/beer/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', views.index, name='beer'),
     path('refresh/', views.refresh, name='refresh'),
     path('nuke/', views.nuke, name='nuke'),
-    path('temp_chart_json/', views.temp_chart_json, name='temp_chart_json'),
-    path('alcohol_chart_json/', views.alcohol_chart_json, name='alcohol_chart_json')
+    path('temp_chart_json', views.temp_chart_json, name='temp_chart_json'),
+    path('alcohol_chart_json', views.alcohol_chart_json, name='alcohol_chart_json')
 ]
