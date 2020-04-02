92c3c144ae28717af6b2f062abe367e9b1a84936
Luke Lau <luke_lau@icloud.com>
2020-04-02 02:16:51 +0100

    Update url

--- websocket-server/main.js
+++ websocket-server/main.js
@@ -1,5 +1,8 @@
-let socket = new WebSocket("wss://0.0.0.0:8080")
-socket.onerror = console.log
+// let socket = new WebSocket("wss://hombrew-iot.appspot.com")
+let socket = new WebSocket("ws://lukelau.me:8080")
+socket.onopen = () => console.log('Connected')
+socket.onclose = () => console.log('Closed')
+socket.onerror = console.error
 socket.onmessage = function (event) {
 	let body = JSON.parse(event.data)
 	document.getElementById("beerTemp").innerHTML = "Beer temperature: " + body.beerTemp
